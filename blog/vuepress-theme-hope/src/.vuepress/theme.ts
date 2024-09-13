852742e1406df1f43e5725df08d0b3f67dfe7cdd
benny <bennyz327@gmail.com>
2024-09-13 11:57:55 +0800

    feat: 更新網站設定

--- blog/vuepress-theme-hope/src/.vuepress/theme.ts
+++ blog/vuepress-theme-hope/src/.vuepress/theme.ts
@@ -4,7 +4,7 @@ import { enNavbar, zhNavbar } from "./navbar/index.js";
 import { enSidebar, zhSidebar } from "./sidebar/index.js";
 
 export default hopeTheme({
-  hostname: "https://blog.benny.pp.ua",
+  hostname: "https://blog.benjk.com",
 
   author: {
     name: "Benny Chou",
